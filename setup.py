ed1db12396f9b8718ab33877102dfebb00c61bb4
Timothy Danford <tdanford@gmail.com>
2014-07-29 05:27:49 -0400

    Adding travis configuration, moving out of the dataclean module.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="datacleaning",
+    version="0.0.1",
+    description="Cleaning utilities for data",
+	author="Timothy Danford",
+	author_email="tdanford@gmail.com",
+	license="Apache 2",
+    py_modules=["datacleaning"],
+    install_requires=[]
+)
